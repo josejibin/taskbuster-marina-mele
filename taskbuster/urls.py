e44572b58114ae74ed8a109dc7a27d9932b12ce7
jibin jose <jibin.jacob13@gmail.com>
2016-05-22 13:04:55 +0530

    Add home view and url

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .views import home
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+]
+
+urlpatterns = [
+  url(r'^$', home, name='home'),
 ]
