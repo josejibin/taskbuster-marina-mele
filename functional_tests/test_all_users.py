5a6974cdc0206dfe3af24c2e7f2dbb5566e699ac
jibin jose <jibin.jacob13@gmail.com>
2016-05-22 13:04:34 +0530

    Add tests

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from django.core.urlresolvers import reverse
+from django.contrib.staticfiles.testing import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class HomeNewVisitotTest(LiveServerTestCase):
 
    def setUp(self):
       self.browser = webdriver.Firefox()
@@ -10,10 +12,19 @@ class NewVisitorTest(unittest.TestCase):
 
    def tearDown(self):
       self.browser.quit()
+
+   def get_full_url(self, namespace):
+      return self.live_server_url + reverse(namespace)
+
+   def test_home_title(self):
+      self.browser.get(self.get_full_url("home"))
+      self.assertIn("TaskBuster", self.browser.title)
   
-   def test_it_worked(self):
-      self.browser.get('http://localhost:8000')
-      self.assertIn('Welcome to Django', self.browser.title)
+   def test_h1_css(self):
+      self.browser.get(self.get_full_url("home"))
+      h1 = self.browser.find_element_by_tag_name("h1")
+      self.assertEqual(h1.value_of_css_property("color"),
+                                  "rgba(200, 550, 255, 1)")
  
 
 if __name__ == '__main__':
